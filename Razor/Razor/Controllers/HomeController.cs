c032354735b861dd2bd3bc94a247efaf7a3f0cea
_TheWeek_ <williansilva1991@gmail.com>
2019-11-29 00:26:07 -0300

    Estudo Razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            ViewBag.Departamentos = ListaDepartamento();
+            return View();
+        }
+
+        public List<Departamento> ListaDepartamento()
+        {
+            List<Categoria> vaListaCategoria = new List<Categoria>();
+            List<Departamento> vaListaDepartamento = new List<Departamento>();
+            Categoria categoria = new Categoria();
+            categoria.Nome = "TV";
+            vaListaCategoria.Add(categoria);
+            categoria = new Categoria();
+            categoria.Nome = "NoteBook";
+            vaListaCategoria.Add(categoria);
+            Departamento departamento = new Departamento();
+            departamento.Nome = "Eletronico";
+            departamento.Categorias = vaListaCategoria;
+            vaListaDepartamento.Add(departamento);
+            return vaListaDepartamento;
+        }
+    }
+}
---
+++ Razor/Razor/Models/Departamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razor.Models
+{
+    public class Departamento
+    {
+        public string Nome { get; set; }
+        public List<Categoria> Categorias { get; set; }
+    }
+}
